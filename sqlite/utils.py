84889fa03bac19c73014eddb71a60127b45905f9
Phoochit Witchutanon <phoochit1574@gmail.com>
2022-10-09 19:34:38 +0700

    add csv result

--- sqlite/utils.py
+++ sqlite/utils.py
@@ -1,36 +1,46 @@
 import re
-import types
 import os
-from datetime import datetime
-from collections import namedtuple 
+from datetime import datetime , timedelta
+from typing import List
 
-def mxl_to_list(xml_file : str) -> None:
+
+def xml_to_list(xml_file: str) -> None:
     """
     create csv file from datalist
     input: datalist
     output: None
     """
-    props = ["EMPID","PASSPORT","FIRSTNAME","LASTNAME","GENDER","BIRTHDAY","NATIONALITY","HIRED","DEPT","POSITION","STATUS","REGION"]
-    with open("data-devclub-1.xml") as f:
-      datafile = f.read()
+    props = ["EMPID", "PASSPORT", "FIRSTNAME", "LASTNAME", "GENDER", "BIRTHDAY",
+             "NATIONALITY", "HIRED", "DEPT", "POSITION", "STATUS", "REGION"]
+    with open(xml_file) as f:
+        datafile = f.read()
     datalist = []
     for prop in props:
         data_prop = re.findall(rf"<{prop}>(.*?)</{prop}>", datafile)
-        if prop in ["HIRED","BIRTHDAY"]:
-            data_prop = [datetime.strptime(datestring, '%d-%m-%Y').strftime('%Y-%m-%d') for datestring in data_prop]
+        if prop in ["HIRED", "BIRTHDAY"]:
+            data_prop = [datetime.strptime(
+                datestring, '%d-%m-%Y').strftime('%Y-%m-%d') for datestring in data_prop]
         datalist.append(data_prop)
     return list(zip(*datalist))
 
-def datalist_to_csv(datalist : list[tuple], csv_file : str) -> None:
-    props = ["EMPID","PASSPORT","FIRSTNAME","LASTNAME","GENDER","BIRTHDAY","NATIONALITY","HIRED","DEPT","POSITION","STATUS","REGION"]
+
+def datalist_to_csv(datalist: List[tuple], csv_file: str) -> None:
+    """
+    create csv file from datalist
+    input: datalist
+    output: None
+    """
+    props = ["EMPID", "PASSPORT", "FIRSTNAME", "LASTNAME", "GENDER", "BIRTHDAY",
+             "NATIONALITY", "HIRED", "DEPT", "POSITION", "STATUS", "REGION"]
     with open(csv_file, 'a') as f:
         f.truncate(0)
         f.write(','.join(props) + '\n')
         for data in datalist:
             f.write(','.join(data) + '\n')
-    
+    print("done !")
 
-def delete_anomaly_status(datalist : list[tuple]) -> list[tuple]:
+
+def delete_anomaly_status(datalist: List[tuple]) -> List[tuple]:
     """Delete anomaly status from datalist
     input: datalist
     output: datalist
@@ -38,22 +48,26 @@ def delete_anomaly_status(datalist : list[tuple]) -> list[tuple]:
     filtered_data = list(filter(lambda x: x[10] != '0', datalist))
     return filtered_data
 
-def filter_positions(datalist : list[tuple], positions : list[str]) -> list[tuple]:
-  filtered_data = list(filter(lambda x : x[9] in positions, datalist))
-  return filtered_data
 
-def filter_hire_time(year : int, datalist : list[tuple]) -> list[tuple]:
+def filter_positions(datalist: List[tuple], positions: List[str]) -> List[tuple]:
+    filtered_data = list(filter(lambda x: x[9] in positions, datalist))
+    return filtered_data
+
+
+def filter_hire_time(year: int, datalist: List[tuple]) -> List[tuple]:
     """
     filter hire time by year
     input: year, datalist
     output: datalist
     """
-    current_time = datetime.datetime.now()
-    base_time = current_time - datetime.timedelta(days=365*year)
-    data_3_years = list(filter(lambda x : datetime.datetime.strptime(x[7], '%Y-%m-%d') < base_time, datalist))
-    len(data_3_years)
+    current_time = datetime.now()
+    base_time = current_time - timedelta(days=365*year)
+    filtered_data = list(filter(lambda x: datetime.strptime(
+        x[7], '%Y-%m-%d') < base_time, datalist))
+    return filtered_data
 
-def _find_duplicate_employee_id(datalist : list[tuple]) -> list[tuple]:
+
+def _find_duplicate_employee_id(datalist: List[tuple]) -> List[tuple]:
     """
     find duplicate employee id
     input: datalist
@@ -71,10 +85,11 @@ def _find_duplicate_employee_id(datalist : list[tuple]) -> list[tuple]:
         else:
             duplicate_data.add(employee_data)
             duplicate_index.append(index)
-        
+
     return duplicate_index
 
-def _find_duplicate_passport(datalist : list[tuple]) -> list[tuple]:
+
+def _find_duplicate_passport(datalist: List[tuple]) -> List[tuple]:
     """
     find duplicate passport
     input: datalist
@@ -92,43 +107,58 @@ def _find_duplicate_passport(datalist : list[tuple]) -> list[tuple]:
         else:
             duplicate_data.add(passport_data)
             duplicate_index.append(index)
-        
+
     return duplicate_index
 
-def remove_duplicate_employee_id(datalist : list[tuple]) -> list[tuple]:
+
+def remove_duplicate_employee_id(datalist: List[tuple]) -> List[tuple]:
     """
     remove duplicate passport data
     input: datalist
     output: datalist
     """
-    filtered_datalist = datalist.copy()
-    for index in sorted(_find_duplicate_employee_id(datalist), reverse=True):
-      del filtered_datalist[index]
-    return filtered_datalist
+    try :
+        filtered_datalist = datalist.copy()
+        for index in sorted(_find_duplicate_employee_id(datalist), reverse=True):
+            del filtered_datalist[index]
+        return filtered_datalist
+    except :
+        pass
 
-def remove_duplicate_passport(datalist : list[tuple]) -> list[tuple]:
+
+def remove_duplicate_passport(datalist: List[tuple]) -> List[tuple]:
     """
     remove duplicate passport data
     input: datalist
     output: datalist
     """
-    filtered_datalist = datalist.copy()
-    for index in sorted(_find_duplicate_passport(datalist), reverse=True):
-      del filtered_datalist[index]
-    return filtered_datalist
+    try :
+        filtered_datalist = datalist.copy()
+        for index in sorted(_find_duplicate_passport(datalist), reverse=True):
+            del filtered_datalist[index]
+        return filtered_datalist
+    except :
+        pass
 
-def Clean_data(datalist : list[tuple]) -> list[tuple]:
+
+def Clean_data(datalist: List[tuple]) -> List[tuple]:
     """
     clean data
     input: datalist
     output: datalist
     """
     filtered_data = delete_anomaly_status(datalist)
-    filtered_data = filter_positions(filtered_data, ['Airhostess','Pilot','Steward'])
-    filtered_data = filter_hire_time(3,filtered_data)
+    print(filtered_data)
+    filtered_data = filter_positions(filtered_data, ['Airhostess', 'Pilot', 'Steward'])
+    print(filtered_data)
+    filtered_data = filter_hire_time(3, filtered_data)
+    print(filtered_data)
     filtered_data = remove_duplicate_employee_id(filtered_data)
+    print(filtered_data)
     filtered_data = remove_duplicate_passport(filtered_data)
+    print(filtered_data)
     return filtered_data
+
 
 def _unique(input_list) -> list:
     unique_list = []
@@ -137,21 +167,23 @@ def _unique(input_list) -> list:
             unique_list.append(x)
     return unique_list
 
-def country_seperate_to_csv(datalist : list[tuple], country : str) -> None:
+
+def country_seperate_to_csv(datalist: List[tuple], save_dir) -> None:
     """
     create csv file seperate by country
     input: datalist, country
     output: None
     """
-    props = ["EMPID","PASSPORT","FIRSTNAME","LASTNAME","GENDER","BIRTHDAY","NATIONALITY","HIRED","DEPT","POSITION","STATUS","REGION"]
+    props = ["EMPID", "PASSPORT", "FIRSTNAME", "LASTNAME", "GENDER", "BIRTHDAY",
+             "NATIONALITY", "HIRED", "DEPT", "POSITION", "STATUS", "REGION"]
 
     country_list = _unique([x[6] for x in datalist])
     for country in country_list:
-      data_list_filter = list(filter(lambda x : x[6] == country, datalist))
-      if not os.path.exists('csv_by_country'):
-        os.makedirs('csv_by_country')
-      with open(f'csv_by_country/output_{country}.csv', 'a') as f :
-        f.truncate(0)
-        f.write(','.join(props) + '\n')
-        for i in data_list_filter:
-          f.write(','.join(i) + '\n')
+        data_list_filter = list(filter(lambda x: x[6] == country, datalist))
+        if not os.path.exists(save_dir):
+            os.makedirs(save_dir)
+        with open(f'{save_dir}/result_{country}.csv', 'a') as f:
+            f.truncate(0)
+            f.write(','.join(props) + '\n')
+            for i in data_list_filter:
+                f.write(','.join(i) + '\n')
